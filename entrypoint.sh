b3fdfdd2345fec21f5b87a7d70cc9aae3e5d97ea
Javier Parada <jpt73@pm.me>
2024-07-26 18:38:55 +0200

    chore: Update entrypoint.sh script for Git mirror secure synchronization

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,11 +16,20 @@ sh -c "git config --global --add safe.directory /github/workspace"
 git config --global user.name  "${MIRROR_GIT_USERNAME}"
 git config --global user.email "${MIRROR_GIT_EMAIL}"
 
+# Setup credentials cache (default cache timeout is 15 minutes)
+git config --global credential.helper cache
+
+# Manually insert credentials into cache
+echo "https://${MIRROR_GIT_USERNAME}:${MIRROR_GIT_TOKEN}@${MIRROR_GIT_HOSTNAME}" | git credential approve
+
 # Checkout the current branch
 git checkout "$GITHUB_REF_NAME"
 
 # Add Mirror Git remote
-git remote add mirror_git "https://${MIRROR_GIT_USERNAME}:${MIRROR_GIT_TOKEN}@${MIRROR_GIT_HOSTNAME}/${MIRROR_GIT_USERNAME}/${MIRROR_GIT_PROJECT}.git"
+git remote add mirror_git "https://${MIRROR_GIT_HOSTNAME}/${MIRROR_GIT_USERNAME}/${MIRROR_GIT_PROJECT}.git"
 
 # Push everything to Mirror Git remote
 git push --force mirror_git
+
+# Cleanup credentials from cache
+echo "url=https://${MIRROR_GIT_HOSTNAME}" | git credential reject
